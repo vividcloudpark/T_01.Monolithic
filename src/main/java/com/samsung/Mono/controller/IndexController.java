e5514458954bbb72d6c2c560dfee99322d8826d7
EC2 Default User <ec2-user@ip-172-31-10-252.us-east-2.compute.internal>
2021-12-24 02:08:25 +0000

    removed unnessary lines

--- src/main/java/com/samsung/Mono/controller/IndexController.java
+++ src/main/java/com/samsung/Mono/controller/IndexController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.ui.Model;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -17,21 +14,13 @@ public class IndexController {
     @RequestMapping("/")
     public String indexView(Model model){
         InetAddress serverip;
-        String hostname;
         try {
             serverip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
             model.addAttribute("serverIP", serverip);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-        
-        
-        HttpServletRequest req = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
-		String ip = req.getHeader("X-FORWARDED-FOR");
-		if (ip == null)
-			ip = req.getRemoteAddr();
-		model.addAttribute("clientIp", ip);
+
         return "index.html";
     }
 
