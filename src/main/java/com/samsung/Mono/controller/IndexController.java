23f9e401ca60a3aafb378c79bb9bbfcdabf67873
EC2 Default User <ec2-user@ip-172-31-10-252.us-east-2.compute.internal>
2021-12-24 01:30:52 +0000

    Added ServerIp

--- src/main/java/com/samsung/Mono/controller/IndexController.java
+++ src/main/java/com/samsung/Mono/controller/IndexController.java
@@ -3,11 +3,35 @@ package com.samsung.Mono.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.ui.Model;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String indexView(){
+    public String indexView(Model model){
+        InetAddress serverip;
+        String hostname;
+        try {
+            serverip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            model.addAttribute("serverIP", serverip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        
+        HttpServletRequest req = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		String ip = req.getHeader("X-FORWARDED-FOR");
+		if (ip == null)
+			ip = req.getRemoteAddr();
+		model.addAttribute("clientIp", ip);
         return "index.html";
     }
 
